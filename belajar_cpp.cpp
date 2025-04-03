f55636bf891dc1159661ccbf64666b9e36a3bbfe
Syarifah Chadijah <syarifah.chadijah06@gmail.com>
2025-04-04 00:00:35 +0700

    Add files via upload

---
+++ belajar_cpp.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+//printing pada console (eps. 6)
+
+// int main ()
+// {
+//     cout << "halo sodara-sodara sekalian\n" ;
+//     cout << "ini adalah baris baru.";
+//     cout << "ini bukan baris baru.";
+//     cout << " ini akhir kalimat." << endl;
+//     return 0;
+// }
+
+// variabel (eps. 7) 
+
+// int main()
+// {
+//     int a = 1;
+//     cout << a << endl;
+//     return 0;
+// }
+
+// deklarasi dan cin (eps. 8)
+
+int main()
+{
+    int a;
+    a = 1;
+    cout << a << endl;
+
+    int b;
+    cout << "masukkan nilainya dong yank: ";
+    cin >> b;
+    cout << "nilai yang kamu masukin adalah: ";
+    cout << b << endl;
+
+    return 0;
+}
